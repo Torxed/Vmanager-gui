e5b3523d19c5d34fdf88aa250739ab954a4013cf
Anton Hvornum <anton.feeds@gmail.com>
2019-11-02 20:14:46 +0100

    No idea, saving working data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #import lzw # Was slightly broken spewing out "EOS" errors.
-import pyglet
+import pyglet, json
 from pyglet.gl import *
 from collections import OrderedDict
 from os import urandom, walk
@@ -16,8 +16,11 @@ from hashlib import sha256
 pyglet.options['audio'] = ('alsa', 'openal', 'silent')
 key = pyglet.window.key
 
+gl_meta = {
+	'viewport' : {'x' : 0, 'y' : 0, 'dx' : 0, 'dy' : 0}
+}
+
 #from config import conf
-#from warlib import *
 
 __builtins__.__dict__['config'] = {
 	'resolution' : {'scale' : 1.0, 'width' : 800, 'height' : 600},
@@ -36,6 +39,8 @@ __builtins__.__dict__['config'] = {
 	'virtual_machines' : {},
 	'virtual_interfaces' : {},
 	'virtual_namespaces' : {},
+
+	'font' : {'size' : 10, 'family' : None}
 }
 
 def genUID():
@@ -60,22 +65,24 @@ def flush_dialogues(*args, **kwargs):
 		config['pages'][config['active_page']]['sprites']['settings']._delete()
 		del(config['pages'][config['active_page']]['sprites']['settings'])
 
-def comp_settings(x, y, **conf):
+def comp_settings(x, y, **configuration):
 	if 'settings' in config['pages'][config['active_page']]['sprites']:
 		config['pages'][config['active_page']]['sprites']['settings']._delete()
 		del(config['pages'][config['active_page']]['sprites']['settings'])
 
-	print(config['virtual_machines'][conf['id']])
+	print(config['virtual_machines'][configuration['id']])
 	config['pages'][config['active_page']]['sprites']['settings'] = menu({'Start' : start_comp, 'Delete' : delete_comp}, x, y)
 
-def copy_template(x, y, **conf):
+def copy_template(x, y, **configuration):
 	_id_ = genUID()
-	config['virtual_machines'][_id_] = conf#config['virtual_machines'][conf['id']]
-	config['pages'][config['active_page']]['sprites'][_id_] = computer(texture=conf['icon'], width=30, height=30, x=x-15, y=y-15, txtmode='under', rbind=comp_settings, rparams={'id' : _id_})
+	config['virtual_machines'][_id_] = configuration#config['virtual_machines'][configuration['id']]
+	config['pages'][config['active_page']]['sprites'][_id_] = computer(texture=configuration['icon'], width=30, height=30, x=x-15, y=y-15, txtmode='under', rbind=comp_settings, rparams={'id' : _id_})
 
-def add_computer(x, y, **conf):
+def add_computer(x, y, **configuration):
 	_id_ = genUID()
-	config['pages'][config['active_page']]['sprites'][_id_] = computer(x=x-15, y=y-15, name=_id_, txtmode='under', rbind=comp_settings, rparams={'id' : _id_})
+	print(x, y)
+	config['pages'][config['active_page']]['sprites'][_id_] = computer(x=x-15-gl_meta['viewport']['x'], y=y-15-gl_meta['viewport']['y'], name=_id_, txtmode='under', rbind=comp_settings, rparams={'id' : _id_})
+
 #	with IPDB() as ip:
 #		config['virtual_interfaces'][_id_] = ip.create(ifname='v0p0', kind='veth', peer='v0p1').commit()
 #		config['virtual_namespaces'][_id_] = {'v0p0' : False, 'v0p1' : True}
@@ -121,7 +128,7 @@ def delete_comp(*args, **kwargs):
 	pass
 
 class generic_sprite(pyglet.sprite.Sprite):
-	def __init__(self, texture=None, width=None, height=None, color="#C2C2C2", alpha=int(0), x=None, y=None, moveable=False, clickable=True, batch=None, group=None, bind=None, bparams={}, rparams={}, rbind=None, dbind=None, txtmode=None, name=None, linkable=False):
+	def __init__(self, texture=None, width=None, height=None, color="#C2C2C2", alpha=int(0), x=None, y=None, moveable=False, clickable=True, batch=None, group=None, bind=None, label='', bparams={}, rparams={}, rbind=None, dbind=None, txtmode=None, name=None, linkable=False, *args, **kwargs):
 		
 		if not width:
 			width = 10
@@ -153,6 +160,7 @@ class generic_sprite(pyglet.sprite.Sprite):
 
 		if not batch:
 			batch = config['pages'][config['active_page']]['batch']
+		self._batch = batch
 
 		if not group:
 			group = 0
@@ -161,7 +169,7 @@ class generic_sprite(pyglet.sprite.Sprite):
 		self.group_nr = group
 		group = config['pages'][config['active_page']]['layers'][group]
 		
-		super(generic_sprite, self).__init__(self.texture, batch=batch, group=group)
+		tmp = super(generic_sprite, self).__init__(self.texture, batch=self._batch, group=group)
 		#self.batch = batch
 		#self.scale = config['resolution']['scale']
 		if scale:
@@ -182,7 +190,7 @@ class generic_sprite(pyglet.sprite.Sprite):
 		self.name = name
 		self.last_click = time()-60
 
-		self.text = ''
+		self.text = label
 		self.txtmode = txtmode
 		if not txtmode or txtmode == 'center':
 			self.txtLabel = pyglet.text.Label(self.text, x=self.x+self.width/2, y=self.y+self.height/2, anchor_x='center', anchor_y='center', batch=self.batch, group=config['pages'][config['active_page']]['layers'][self.group_nr+1] if group else None)
@@ -227,6 +235,8 @@ class generic_sprite(pyglet.sprite.Sprite):
 
 	def _delete(self):
 		self.txtLabel.delete()
+		for sprite in self.sprites:
+			self.sprites[sprite]._delete()
 		self.delete()
 
 	def dummy(self, *args, **kwargs):
@@ -301,9 +311,13 @@ class generic_sprite(pyglet.sprite.Sprite):
 		if self.moveable:
 			self.x += x
 			self.y += y
+			self.txtLabel.x += x
+			self.txtLabel.y += y
 			for sprite in self.sprites:
-				self.sprites[sprite].x += x
-				self.sprites[sprite].y += y
+				self.sprites[sprite].move(x, y)#.x += x
+				#self.sprites[sprite].y += y
+				#self.sprites[sprite].txtLabel.x += x
+				#self.sprites[sprite].txtLabel.y += y
 
 			for index, (wif, line) in self.links.items():
 				line.update_origin(self.x+(self.width/2), self.y+(self.height/2))
@@ -325,6 +339,13 @@ class generic_sprite(pyglet.sprite.Sprite):
 				self.txtLabel.x=self.x+self.width/2
 				self.txtLabel.y=self.y-12
 
+	def add_subobj(self, obj, name=genUID(), *args, **kwargs):
+		self.sprites[name] = obj(batch=self._batch, *args, **kwargs)
+
+	def remove(self, obj_name):
+		if obj_name in self.sprites:
+			self.sprites[obj_name]._delete()
+
 #	def click(self, x, y):
 #		return True
 
@@ -333,7 +354,7 @@ class generic_sprite(pyglet.sprite.Sprite):
 		self.txtLabel.draw()
 
 class computer(generic_sprite):
-	def __init__(self, x=0, y=0, **kwargs):
+	def __init__(self, x=0, y=0, *args, **kwargs):
 		if not 'moveable' in kwargs:
 			kwargs['moveable'] = True
 		if not 'txtmode' in kwargs:
@@ -342,7 +363,7 @@ class computer(generic_sprite):
 			kwargs['texture'] = gen_solid_image(30, 30, color='#00FF00')
 		if not 'linkable' in kwargs:
 			kwargs['linkable'] = True
-		generic_sprite.__init__(self, group=2, x=x, y=y, dbind=self.doubleClick, **kwargs)
+		generic_sprite.__init__(self, group=2, x=x, y=y, dbind=self.doubleClick, rbind=self.rightClick, **kwargs)
 		self.text = 'Computer'
 		self.started = False
 		self.tap = None
@@ -357,6 +378,28 @@ class computer(generic_sprite):
 			self.sprites['border'].delete()
 			del(self.sprites['border'])
 			self.started = False
+
+	def start(self, *args, **kwargs):
+		self.doubleClick(0,0)
+
+	def stop(self, *args, **kwargs):
+		self.doubleClick(0,0)
+
+	def start_stop(self, *args, **kwargs):
+		self.start() if not self.started else self.stop()
+		self.remove('rmenu')
+
+	def open_settings(self, *args, **kwargs):
+		print('Opening settings dialoge for', self)
+		self.remove('rmenu')
+
+	def rightClick(self, x, y):
+		menu_options = {
+			'Start' if not self.started else 'Stop' : self.start_stop,
+			'Settings' : self.open_settings
+		}
+		self.add_subobj(menu, 'rmenu', label='Menu: {}'.format(self), x=self.x, y=self.y, menu_options=menu_options)
+
 
 class switch(generic_sprite):
 	def __init__(self, x=0, y=0, **kwargs):
@@ -403,41 +446,35 @@ class linker(generic_sprite):
 		if self.clickAction:
 			self.clickAction(x, y)
 
+
+
+class menu_entry(generic_sprite):
+	def __init__(self, x=0, y=0, width=30, bind=None, **kwargs):
+		if not 'moveable' in kwargs:
+			kwargs['moveable'] = True
+
+		self.backdrop = gen_solid_image(width, 30, color='#FF3333')
+		generic_sprite.__init__(self, bind=bind, group=0, texture=self.backdrop, x=x, y=y+10, txtmode='left', clickable=True, **kwargs)
+
+	def lClick(self, x, y, *args, **kwargs):
+		print('Clicked menu at {}x{}'.format(x,y))
+
 class menu(generic_sprite):
 	def __init__(self, menu_options, x=0, y=0, **kwargs):
 		if not 'moveable' in kwargs:
-			kwargs['moveable'] = False
+			kwargs['moveable'] = True
 
-		self.off = gen_solid_image(30, 30, color='#333333')
-		generic_sprite.__init__(self, group=0, texture=self.off, x=x, y=y, txtmode='center', **kwargs)
-		self.text = 'Right Click Menu'
+		self.backdrop = gen_solid_image((len(kwargs['label'])*config['font']['size']), 30, color='#FF3333')
+		generic_sprite.__init__(self, bind=self.lClick, group=0, texture=self.backdrop, x=x+40, y=y+10, txtmode='center', clickable=True, **kwargs)
 
-config['pages'][config['active_page']]['sprites']['comp1'] = computer(x=0, y=config['resolution']['height']-30, bind=add_computer, moveable=False, txtmode='left')
-config['pages'][config['active_page']]['sprites']['router1'] = router(x=0, y=config['resolution']['height']-60, bind=add_router, moveable=False, txtmode='left')
-config['pages'][config['active_page']]['sprites']['switch1'] = switch(x=0, y=config['resolution']['height']-90, bind=add_switch, moveable=False, txtmode='left')
-config['pages'][config['active_page']]['sprites']['linkButton'] = linker(x=0, y=config['resolution']['height']-120, moveable=False)
+		y = self.y-30
+		for label in menu_options:
+			self.add_subobj(menu_entry, 'menu_entry_{}'.format(label), bind=menu_options[label], width=self.width, label=label, x=self.x, y=y)
+			y -= 30
 
-x = 31
-for root, folders, files in walk('./'):
-	for filename in files:
-		if not '.template' in filename:
-			continue
+	def lClick(self, x, y, *args, **kwargs):
+		print('Clicked menu at {}x{}'.format(x,y))
 
-		with open(filename) as template:
-			conf = loads(template.read())
-
-		name = filename.rsplit('.',1)[0]
-		if not 'icon' in conf or not isfile(abspath(conf['icon'])):
-			if isfile(abspath('./icon_'+name+'.png')):
-				conf['icon'] = abspath('./'+name+'.png')
-			else:
-				conf['icon'] = None
-
-		config['pages'][config['active_page']]['sprites']['comp_'+name] = computer(texture=conf['icon'], x=x, y=config['resolution']['height']-30, bind=copy_template, bparams=conf, moveable=False, linkable=False, rbind=comp_settings, rparams={'name' : 'comp_'+name})
-		config['pages'][config['active_page']]['sprites']['comp_'+name].text = ''
-		config['pages'][config['active_page']]['sprites']['comp_'+name].scale = min(30.0/config['pages'][config['active_page']]['sprites']['comp_'+name].image.width, 30.0/config['pages'][config['active_page']]['sprites']['comp_'+name].image.height)
-		config['virtual_machines'][config['pages'][config['active_page']]['sprites']['comp_'+name].name] = conf
-		x += 31
 def dummy(*args, **kwargs):
 	pass
 
@@ -445,6 +482,13 @@ class generic_shape():
 	def __init__(self, batch, *args, **kwargs):
 		#VertexList.__init__(self, *args, **kwargs)
 		self.vertices = batch.add(*args, **kwargs)
+		self.batch = batch
+		group = args[2]
+		if not group:
+			group = 0
+		if type(group) != int:
+			raise ValueError('Group number needs to be of type INT (or None) in order to function.')
+		self.group_nr = group
 		self.clickable = False
 
 	def dummy(self, *args, **kwargs):
@@ -472,8 +516,14 @@ class generic_shape():
 		pass
 
 class line(generic_shape):
-	def __init__(self, batch, sx, sy, ex, ey, color=(255,255,255,255)):
-		generic_shape.__init__(self, batch, 2, GL_LINES, None, ("v2f", (sx, sy, ex, ey)), ('c4B', color*2))
+	def __init__(self, batch, sx, sy, ex, ey, color=(255,255,255,255), group=None):
+		generic_shape.__init__(self, batch, 2, GL_LINES, group, ("v2f", (sx, sy, ex, ey)), ('c4B', color*2))
+		self.x = sx
+		self.y = sy
+		self.width = max(sx, ex) - min(sx, ex)
+		self.height = max(sy, ey) - min(sy, ey)
+		self.text = ''
+		self.txtLabel = pyglet.text.Label(self.text, x=self.x+self.width/2, y=self.y+self.height/2, anchor_x='center', anchor_y='center', batch=self.batch, group=config['pages'][config['active_page']]['layers'][self.group_nr+1] if group else None)
 
 	def update_destination(self, x, y):
 		#print(dir(self.vertices))
@@ -499,6 +549,13 @@ class main(pyglet.window.Window):#								 config['resolution'].height()
 
 		self.drag = False
 		self.alive = 1
+		glDepthFunc(GL_LEQUAL)
+		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+
+		glViewport(0, 0, config['resolution']['width'], config['resolution']['height'])
+		glOrtho(0, 2, 0, 2, -1, 1)
+		#glFrustum(0,1, 0,1, 0,1)
+		glMatrixMode(GL_MODELVIEW)
 
 	def on_draw(self):
 		self.render()
@@ -544,10 +601,18 @@ class main(pyglet.window.Window):#								 config['resolution'].height()
 					#glEnd()
 	#				pyglet.graphics.draw(2, GL_LINES, ('v2i', (self.drag_origin[0], self.drag_origin[1], x, y)))
 		else:
-			for name, obj in self.active_sprites.items():
-				obj.move(dx, dy)
-				if not key.LSHIFT in self.keys:
-					break
+			if len(self.active_sprites):
+				for name, obj in self.active_sprites.items():
+					obj.move(dx, dy)
+					if not key.LSHIFT in self.keys:
+						break
+			else:
+				gl_meta['viewport']['x'] += dx
+				gl_meta['viewport']['y'] += dy
+				gl_meta['viewport']['dx'] += dx
+				gl_meta['viewport']['dy'] += dy
+
+				#glTranslatef(0, 0, 0.1)
 
 	def on_key_release(self, symbol, modifiers):
 		if symbol == key.LCTRL:
@@ -625,8 +690,15 @@ class main(pyglet.window.Window):#								 config['resolution'].height()
 
 		for sName, sObj in config['pages'][config['active_page']]['sprites'].items():
 			sObj.update()
+
+		#glViewport(gl_meta['viewport']['x'], gl_meta['viewport']['y'], config['resolution']['width']+gl_meta['viewport']['x'], config['resolution']['height']+gl_meta['viewport']['y'])
 		config['pages'][config['active_page']]['batch'].draw()
 
+		#glViewport(0, 0, config['resolution']['width'], config['resolution']['height'])
+		config['pages'][config['active_page']]['menu'].draw()
+		
+		gl_meta['viewport']['dx'] = 0
+		gl_meta['viewport']['dy'] = 0
 		self.flip()
 
 	def run(self):
@@ -640,6 +712,66 @@ class main(pyglet.window.Window):#								 config['resolution'].height()
 			event = self.dispatch_events()
 
 		self.on_close()
+
+config['pages'][config['active_page']]['menu'] = pyglet.graphics.Batch()
+config['pages'][config['active_page']]['sprites']['btn_computer'] = computer(x=0, y=config['resolution']['height']-30, bind=add_computer, moveable=False, txtmode='left', batch=config['pages'][config['active_page']]['menu'])
+config['pages'][config['active_page']]['sprites']['btn_router'] = router(x=0, y=config['resolution']['height']-60, bind=add_router, moveable=False, txtmode='left', batch=config['pages'][config['active_page']]['menu'])
+config['pages'][config['active_page']]['sprites']['btn_switch'] = switch(x=0, y=config['resolution']['height']-90, bind=add_switch, moveable=False, txtmode='left', batch=config['pages'][config['active_page']]['menu'])
+config['pages'][config['active_page']]['sprites']['linkButton'] = linker(x=0, y=config['resolution']['height']-120, moveable=False, batch=config['pages'][config['active_page']]['menu'])
+
+struct = {
+	'computers' : {
+		'computer_1' : {'x' : 279, 'y' : 367, 'type' : 'computer'},
+		'computer_2' : {'x' : 483, 'y' : 350, 'type' : 'computer'}
+	},
+	'switches' : {
+		'switch_1' : {'x' : 370, 'y' : 215, 'type' : 'switch'}
+	},
+	'routers' : {},
+	'links' : {
+		'computer_1' : ['switch_1'],
+		'computer_2' : ['switch_1']
+	}
+}
+
+for uuid in struct['computers']:
+	config['pages'][config['active_page']]['sprites'][uuid] = computer(**struct['computers'][uuid])
+
+for uuid in struct['switches']:
+	config['pages'][config['active_page']]['sprites'][uuid] = switch(**struct['switches'][uuid])
+
+for link_src in struct['links']:
+	if link_src in config['pages'][config['active_page']]['sprites']:
+		for link_dst in struct['links'][link_src]:
+			if link_dst in config['pages'][config['active_page']]['sprites']:
+				print('Linking {} with {}'.format(link_src, link_dst))
+				config['pages'][config['active_page']]['sprites'][link_src].link(config['pages'][config['active_page']]['sprites'][link_dst])
+			else:
+				print('Can not link {} with {}'.format(link_src, link_dst))
+	else:
+		print('Link source {} not in sprites.'.format(link_src))
+
+#x = 31
+#for root, folders, files in walk('./'):
+#	for filename in files:
+#		if not '.template' in filename:
+#			continue
+#
+#		with open(filename) as template:
+#			configuration = loads(template.read())
+#
+#		name = filename.rsplit('.',1)[0]
+#		if not 'icon' in configuration or not isfile(abspath(configuration['icon'])):
+#			if isfile(abspath('./icon_'+name+'.png')):
+#				configuration['icon'] = abspath('./'+name+'.png')
+#			else:
+#				configuration['icon'] = None
+#
+#		config['pages'][config['active_page']]['sprites']['comp_'+name] = computer(texture=configuration['icon'], x=x, y=config['resolution']['height']-30, bind=copy_template, bparams=configuration, moveable=False, linkable=False, rbind=comp_settings, rparams={'name' : 'comp_'+name})
+#		config['pages'][config['active_page']]['sprites']['comp_'+name].text = ''
+#		config['pages'][config['active_page']]['sprites']['comp_'+name].scale = min(30.0/config['pages'][config['active_page']]['sprites']['comp_'+name].image.width, 30.0/config['pages'][config['active_page']]['sprites']['comp_'+name].image.height)
+#		config['virtual_machines'][config['pages'][config['active_page']]['sprites']['comp_'+name].name] = configuration
+#		x += 31
 
 x = main()
 x.run()

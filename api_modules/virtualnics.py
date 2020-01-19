264c344c4e36cb86ec5830f20ab1e5d4b53b0b73
Anton Hvornum <anton.feeds+github@gmail.com>
2020-01-19 22:06:49 +0100

    Fixed the structure of virtual nics in the virtualnic API

--- api_modules/virtualnics.py
+++ api_modules/virtualnics.py
@@ -5,8 +5,12 @@ def get_overview():
 	response = {'nics' : {}}
 	for nic_name in vmanager.nics:
 		response['nics'][nic_name] = {
-			'port' : vmanager.nics[nic_name].ports['source_name'],
-			'sink' : vmanager.nics[nic_name].ports['sink_name']
+			'ip' : vmanager.nics[nic_name].ip,
+			'MAC' : vmanager.nics[nic_name].mac,
+			'state' : vmanager.nics[nic_name].state,
+			'gateway' : vmanager.nics[nic_name].ports['sink_name'],
+			'routes' : None,
+			'connected_to' : None
 		}
 
 	for nic_name in vmanager.interfaces:
